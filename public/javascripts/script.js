3f0f45db0e8ccde11c331b2f964db11d9c800bdf
j-beckman <beckman.joshua@gmail.com>
2014-01-15 02:02:21 -0600

    Getting shit together

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -15,6 +15,56 @@
   }
 })(this, this.document);
 
+// Init demo
+(function(window, document){
+  var btn = document.getElementById('demo-button');
+  function makeDemoRun () {
+    return function() {
+      var input = document.getElementById('demo-input'),
+          btn = document.getElementById('demo-button'),
+          img = document.getElementById('demo-image'),
+          elem = document.getElementById('demo-resp'),
+          url = window.location.origin + '/id?i=' + input.value;
+      img.src = input.value;
+      elem.innerHTML = 'Running...';
+      btn.innerHTML = 'Running...';
+      request(url, function(err,data,xhr){
+        handleDemoResponse(JSON.parse(data), elem);
+        btn.innerHTML = 'Analyze Colors';
+      })
+    };
+  }
+  btn.onclick = makeDemoRun();
+  function handleDemoResponse (data, elem) {
+    if (data.query) {
+      var length = data.colors.length,
+        i = 0;
+      elem.innerHTML = '<p>File size: <code>' + data.identity['Filesize'].toString() + '</code><br>Elapsed time: <code>' + data.query['elapsed time'].toString() + ' ms</code><br>Pixels per second: <code>' + data.identity['Pixels per second'].toString() + '</code><br>Date created: <code>' + data.identity.Properties['date:create'].toString() + '</code><br>Gamma: <code>' + data.identity.Gamma.toString() + '</code><br><em>...etc...</em></p>';
+      for (; i < length; i++) {
+        passColor(elem, data.colors[i].hex, (30 * i));
+      }
+    } else {
+      html  = '<h3 class="error">Error:</h3>';
+      html += '<code>' + data + '</code>';
+      elem.innerHTML = html;
+    }
+    function passColor (elem, color, wait) {
+      setTimeout(function(){elem.innerHTML += '<span class="demo-color" style="background-color:' + color + ';"></span>';}, wait);
+    }
+  }
+  var demoUrls = document.getElementsByClassName('demo-url'),
+    leng = demoUrls.length,
+    i = 0;
+  function makeDemoUrlListen (url) {
+    return function(){
+      document.getElementById('demo-input').value = url;
+    };
+  }
+  for (; i < leng; i++) {
+    demoUrls[i].onclick = makeDemoUrlListen(demoUrls[i].dataset.demoUrl);
+  };
+})(this, this.document);
+
 // Custom ScrollTo
 window.scrollTo = function(element, to, duration) {
   var start = element.scrollY,
@@ -38,3 +88,26 @@ Math.easeInOutQuad = function (t, b, c, d) {
   t--;
   return -c/2 * (t*(t-2) - 1) + b;
 };
+// Custom Request
+var request = function(url,cb,method,post,contenttype){
+var requestTimeout,xhr;
+try{ xhr = new XMLHttpRequest(); }catch(e){
+try{ xhr = new ActiveXObject("Msxml2.XMLHTTP"); }catch (error){
+if(console)console.log("tinyxhr: XMLHttpRequest not supported");
+return null;
+}
+}
+requestTimeout = setTimeout(function() {xhr.abort(); cb(new Error("tinyxhr: aborted by a timeout"), "",xhr); }, 10000);
+xhr.onreadystatechange = function(){
+if (xhr.readyState != 4) return;
+clearTimeout(requestTimeout);
+cb(xhr.status != 200?new Error("tinyxhr: server respnse status is "+xhr.status):false, xhr.responseText,xhr);
+};
+xhr.open(method?method.toUpperCase():"GET", url, true);
+if(!post){
+xhr.send();
+}else{
+xhr.setRequestHeader('Content-type', contenttype?contenttype:'application/x-www-form-urlencoded');
+xhr.send(post);
+}
+}
--- readme.md
+++ readme.md
@@ -1,2 +1,6 @@
-# Imogen says "Hi."
+# [Aislin](http://www.aislin.co) says "Hi."
 ---
+
+__Wake up to all the data in your images.__
+
+The best image analysis API out there.
