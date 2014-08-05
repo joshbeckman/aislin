85f2763089e8b7af9a6a2c7e487280fd4eca5c31
j-beckman <beckman.joshua@gmail.com>
2014-08-04 22:57:11 -0500

    foo

--- app.js
+++ app.js
@@ -24,7 +24,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'jade');
 app.use(express.favicon());
 app.use(express.logger('dev'));
-app.use(cachingMiddleware(120000,{'type':'application/json','driver':'memjs'}));
+// app.use(cachingMiddleware(120000,{'type':'application/json','driver':'memjs'}));
 app.use(express.bodyParser());
 app.use(express.methodOverride());
 app.use(app.router);
